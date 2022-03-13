fe7d2f465154c0672ab6c1c3992955bd32dfbe3c
Petr Miko <miko.petr@gmail.com>
2022-03-13 13:42:39 +0100

    Fixed style for forgotten 404

--- client/src/components/not-found/index.jsx
+++ client/src/components/not-found/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const NotFound = () => <div className=''>
-	<div className=''>This is not the page you&apos;re looking for.</div>
-	<div className=''>You should return to <a href='/'>main page</a>. Nothing to be seen here.</div>
+const NotFound = () => <div className='text-center'>
+	<div className='text-2xl font-bold'>This is not the page you&apos;re looking for.</div>
+	<div className='text-xl'>You should return to <a href='/' className='underline hover:no-underline'>main page</a>. Nothing to be seen here.</div>
 </div>
 
 export default NotFound
