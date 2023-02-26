b8caa28336ef4665d968d32b20b6ebf2ec7a85c3
Petr Miko <miko.petr@gmail.com>
2023-02-26 18:42:07 +0100

    Applying i18n and localeDetection only of there are some domains in .env

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
+const domains = [
+	process.env.DOMAIN_CZ && {
+		domain: process.env.DOMAIN_CZ,
+		defaultLocale: 'cs-CZ',
+	},
+	process.env.DOMAIN_EN &&{
+		domain: process.env.DOMAIN_EN,
+		defaultLocale: 'en-US',
+	},
+].filter(Boolean)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	i18n: {
 		locales: ['cs-CZ', 'en-US'],
 		defaultLocale: 'cs-CZ',
-		localeDetection: false,
-		domains: [
-			{
-			  domain: process.env.DOMAIN_CZ,
-			  defaultLocale: 'cs-CZ',
-			},
-			{
-			  domain: process.env.DOMAIN_EN,
-			  defaultLocale: 'en-US',
-			},
-		],
+		...(domains.length && {
+			domains,
+			localeDetection: false,
+		})
 	}
 }
 
